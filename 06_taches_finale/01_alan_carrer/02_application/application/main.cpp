f5af0e1ab320883ddc679c590b4c2d987a6fa0ee
Izakun <carrer.a@outlook.fr>
2018-03-23 20:36:16 +0100

    application
    update

--- 06_taches_finale/01_alan_carrer/02_application/application/main.cpp
+++ 06_taches_finale/01_alan_carrer/02_application/application/main.cpp
@@ -4,8 +4,8 @@
 #include <iostream>
 #include "mainwindow.h"
 #include "ceolienne.h"
-//#include "cscenario.h"
-//#include "cphase.h"
+#include "cscenario.h"
+#include "cphase.h"
 
 int main(int argc, char *argv[])
 {
@@ -14,11 +14,11 @@ int main(int argc, char *argv[])
 
     // initialization database connection
     QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
-    db.setHostName("localhost"); // localhost | 127.0.0.1 | 10.16.2.147
+    db.setHostName("127.0.0.1"); // localhost | 127.0.0.1 | 10.16.2.147
     db.setPort(3306);
     db.setDatabaseName("test");
     db.setUserName("root");
-    db.setPassword("");
+    db.setPassword("root");
 
     if(db.isValid()) std::cout << "valid" << std::endl;
     else std::cout << "unvalid" << std::endl;
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
     if(db.open()) std::cout << "open" << std::endl;
     else std::cout << "close" << std::endl;
 
-    Ceolienne e1("test","test1","test2","test3");
+    Ceolienne e1("test","test1","test2","test4");
     e1.insertDB(db);
 
     w.show();
